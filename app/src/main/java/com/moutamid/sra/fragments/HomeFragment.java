523206fe0950d8b6610af2cb63059ff7fe14f52b
suleman <sunnybajwa904@gmail.com>
2022-12-28 13:11:56 +0500

    commit

--- app/src/main/java/com/moutamid/sra/fragments/HomeFragment.java
+++ app/src/main/java/com/moutamid/sra/fragments/HomeFragment.java
@@ -47,7 +47,7 @@ public class HomeFragment extends Fragment {
     String[] taskNames = {"Captcha", "Amazon", "Dummy Task 1", "Dummy Task 2"};
     int[] taskAmount = {50, 100, 150, 200};
     int[] taskIncome = {10, 30, 50, 100};
-    int[] taskImages = {R.drawable.captcha, R.drawable.amazon, R.drawable.logo_bg, R.drawable.logo_bg};
+    int[] taskImages = {R.drawable.captcha, R.drawable.amazon, R.drawable.sra_logo, R.drawable.sra_logo};
     boolean[] taskLockState = {false, true, true, true};
     TasksAdapter adapter;
     List<TasksModel> list;
