e5e7bbd5f8a97ce67ef3bee3d52766b0cdbb6f78
suleman81 <sunnybajwa904@gmail.com>
2023-01-18 16:53:47 +0500

    commit

--- app/src/main/java/com/moutamid/sra/RulesActivity.java
+++ app/src/main/java/com/moutamid/sra/RulesActivity.java
@@ -3,17 +3,31 @@ package com.moutamid.sra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.moutamid.sra.databinding.ActivityRulesBinding;
+import com.moutamid.sra.utils.Constants;
 
 public class RulesActivity extends AppCompatActivity {
-
+    ActivityRulesBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rules);
+        binding = ActivityRulesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         findViewById(R.id.back).setOnClickListener(v -> {
             onBackPressed();
             finish();
+        });
+
+        binding.rules.setText("");
+
+        Constants.databaseReference().child("rules").get().addOnSuccessListener(dataSnapshot -> {
+            String s = dataSnapshot.child("rules").getValue().toString();
+            binding.rules.setText(s);
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         });
 
     }
